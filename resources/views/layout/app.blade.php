05752313c9b1e6ad30bdf13883207eabe5980e1a
oluwaCyna <oluwashinaayomi2019@gmail.com>
2023-02-14 05:02:42 -0800

    done, testing

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -11,7 +11,7 @@
 
     <!-- Fonts -->
     <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <!-- Styles -->
     <style>
         body {
@@ -52,6 +52,7 @@
         @yield('content')
     </main>
     @yield('script')
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
 </body>
 
 </html>
